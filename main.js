19206acdb73f19a07b36ff20b400d235262b1c63
Kurt Kotzur <kurtgeorge@gmail.com>
2015-02-10 10:56:27 -0800

    split presentation login into GameView

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+var ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+
 var Game = function(ctx) {
   this.ctx = ctx;
   this.letters = [];
@@ -5,54 +7,34 @@ var Game = function(ctx) {
   this.letterCount = 0;
   this.newLetterInterval = 10;
   this.frameCount = 0;
+  this.over = false;
 };
 
 Game.DIM_X = 1000;
 Game.DIM_Y = Math.max(document.documentElement.clientHeight, window.innerHeight || 0) - 20;
 Game.FPS = 10;
 Game.FONT = '20px sans-serif';
-Game.ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
 
 Game.prototype.addLetter = function() {
   this.letters.push(new Letter({
     game: this,
-    str: Game.ALPHABET[Math.floor(26 * Math.random())],
+    str: ALPHABET[Math.floor(26 * Math.random())],
     pos: this.randomPosition(),
     vel: 10
   }));
 }
 
-Game.prototype.bindKeyHandlers = function() {
-  var game = this;
-  document.addEventListener('keyup', this.keyHandler.bind(this));
-};
-
-Game.prototype.handleLetterKeyPress = function(keyCode) {
-  var str = Game.ALPHABET[keyCode - 65],
-      delta = this.removeOldestInstance(str) ? 1 : -1;
-  this.updateScore(delta);
-};
-
-Game.prototype.draw = function() {
+Game.prototype.draw = function(ctx) {
   ctx.clearRect(0, 0, Game.DIM_X, Game.DIM_Y);
   var game = this;
   this.letters.forEach(function(letter) {
-    letter.draw(game.ctx);
+    letter.draw(ctx);
   });
-  this.showScore();
+  this.showScore(ctx);
 };
 
 Game.prototype.isOutOfBounds = function(pos) {
   return pos[0] > Game.DIM_X;
-};
-
-Game.prototype.keyHandler = function(event) {
-  var keyCode = event.keyCode;
-  if (65 <= keyCode && keyCode <= 90) {
-    this.handleLetterKeyPress(keyCode);
-  } else if (keyCode === 27) {
-    this.stop();
-  }
 };
 
 Game.prototype.randomPosition = function() {
@@ -64,32 +46,20 @@ Game.prototype.randomPosition = function() {
 }
 
 Game.prototype.removeOldestInstance = function(str) {
-  var oldestIndex = false;
+  var oldestIndex = false, delta = -1;
   this.letters.some(function(letter, i) {
     if (letter.str === str) {
       oldestIndex = i;
+      delta = 1;
       return true;
     }
   });
-  if (oldestIndex !== false) {
-    this.letters.splice(oldestIndex, 1);
-    return true;
-  } else {
-    return false;
-  }
+  if (oldestIndex !== false) this.letters.splice(oldestIndex, 1);
+  this.updateScore(delta);
 };
 
-Game.prototype.showScore = function() {
-  this.ctx.fillText('Score:' + this.score.toString(), Game.DIM_X - 100, 20);
-};
-
-Game.prototype.start = function() {
-  var game = this;
-  this.timerId = setInterval(function() {
-    game.step();
-    game.draw();
-  }, 1000 / Game.FPS);
-  this.bindKeyHandlers();
+Game.prototype.showScore = function(ctx) {
+  ctx.fillText('Score:' + this.score.toString(), Game.DIM_X - 100, 20);
 };
 
 Game.prototype.step = function() {
@@ -103,11 +73,6 @@ Game.prototype.step = function() {
   }
 };
 
-Game.prototype.stop = function() {
-  clearInterval(this.timerId);
-  alert('Game over.');
-};
-
 Game.prototype.updateScore = function(delta) {
   this.score += delta;
   if (delta === 1) {
@@ -116,6 +81,50 @@ Game.prototype.updateScore = function(delta) {
   if (this.letterCount % 20 === 0) {
     this.newLetterInterval--;
   }
+};
+
+var GameView = function(game, ctx) {
+  this.ctx = ctx;
+  this.game = game;
+  this.timerId = null;
+};
+
+
+GameView.prototype.bindKeyHandlers = function() {
+  document.addEventListener('keyup', this.keyHandler.bind(this));
+};
+
+GameView.prototype.checkGameState = function() {
+  if (this.game.over) this.stop();
+}
+
+GameView.prototype.handleLetterKeyPress = function(keyCode) {
+  var str = ALPHABET[keyCode - 65];
+  this.game.removeOldestInstance(str);
+};
+
+GameView.prototype.keyHandler = function(event) {
+  var keyCode = event.keyCode;
+  if (65 <= keyCode && keyCode <= 90) {
+    this.handleLetterKeyPress(keyCode);
+  } else if (keyCode === 27) {
+    this.stop();
+  }
+};
+
+GameView.prototype.start = function() {
+  var gameView = this;
+  this.timerId = setInterval(function() {
+    gameView.game.step();
+    gameView.checkGameState();
+    gameView.game.draw(gameView.ctx);
+  }, 1000 / Game.FPS);
+  this.bindKeyHandlers();
+};
+
+GameView.prototype.stop = function() {
+  clearInterval(this.timerId);
+  alert('Game over.');
 };
 
 var Letter = function(options) {
@@ -132,6 +141,6 @@ Letter.prototype.draw = function(ctx) {
 Letter.prototype.move = function() {
   this.pos[0] += this.vel;
   if (this.game.isOutOfBounds(this.pos)) {
-    this.game.stop();
+    this.game.over = true;
   }
 };
